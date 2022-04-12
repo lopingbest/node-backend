9f0e346a2955e73c5d97d9ae2f36cb813ed46023
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-04-12 22:12:49 +0700

    feat(CRUD API): Add create routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 const route = require('express').Router();
 
-const {listUser} = require('../controller/users')
+const {listUser,createUser} = require('../controller/users')
 
 // Can access controller which attach data from model, model attach data from database
 route.get('/', listUser)
+route.post('/', createUser)
 
 module.exports = route
