c48b9d466bb91fa1d8b8f0affd9f01785dc66c84
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-04-13 08:53:19 +0700

    feat(CRUD API): Add update routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,10 @@
 const route = require('express').Router();
 
-const {listUser,createUser} = require('../controller/users')
+const {listUser,createUser, updateUser} = require('../controller/users')
 
 // Can access controller which attach data from model, model attach data from database
 route.get('/', listUser)
 route.post('/', createUser)
+route.patch('/:id', updateUser)
 
 module.exports = route
