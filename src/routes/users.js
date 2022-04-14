8ff89cd8c78b816f64fc1cd533874dbebf2deea4
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-04-14 23:23:48 +0700

    feat(CRUD API): Add delete routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 const route = require('express').Router();
 
-const {listUser,createUser, updateUser} = require('../controller/users')
+const {listUser,createUser,updateUser,deleteUser} = require('../controller/users')
 
 // Can access controller which attach data from model, model attach data from database
 route.get('/', listUser)
 route.post('/', createUser)
 route.patch('/:id', updateUser)
+route.delete('/:id', deleteUser)
 
 module.exports = route
